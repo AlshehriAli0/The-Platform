023a232e160fdafd9242aabaa7a80b26f4a6ebcb
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-17 16:10:58 +0300

    chore: set avatar string to default user image

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,7 +1,8 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
+import createClient from "@/utils/supabase/server";
 
-import  createClient  from "@/utils/supabase/server";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -9,8 +10,7 @@ export async function GET(request: NextRequest) {
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
 
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
+  const redirectTo = new URL(next, BASE_URL);
   redirectTo.searchParams.delete("token_hash");
   redirectTo.searchParams.delete("type");
 
@@ -24,10 +24,10 @@ export async function GET(request: NextRequest) {
     if (!error) {
       redirectTo.pathname = "/auth/complete-signup";
       redirectTo.searchParams.delete("next");
-      return NextResponse.redirect(redirectTo);
+      return NextResponse.redirect(redirectTo.toString());
     }
   }
 
   redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo);
+  return NextResponse.redirect(redirectTo.toString());
 }
