7559f555838fffb79cf20fcc09f1257c345df67e
Ali Alshehri <Ali0fawzish@outlook.com>
2024-06-17 16:14:25 +0300

    revert route.ts

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,8 +1,7 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
-import createClient from "@/utils/supabase/server";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+import  createClient  from "@/utils/supabase/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -10,7 +9,8 @@ export async function GET(request: NextRequest) {
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
 
-  const redirectTo = new URL(next, BASE_URL);
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
   redirectTo.searchParams.delete("token_hash");
   redirectTo.searchParams.delete("type");
 
@@ -24,10 +24,10 @@ export async function GET(request: NextRequest) {
     if (!error) {
       redirectTo.pathname = "/auth/complete-signup";
       redirectTo.searchParams.delete("next");
-      return NextResponse.redirect(redirectTo.toString());
+      return NextResponse.redirect(redirectTo);
     }
   }
 
   redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo.toString());
+  return NextResponse.redirect(redirectTo);
 }
